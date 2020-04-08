c8f493f02d0681bad00c224597018d0940ebfa0a
Vasyl Kitsyla <wkichira@gmail.com>
2020-04-08 17:19:05 +0300

    Update views.py
    created class MovieList which brings movies to the site

--- planetamovies/movies/views.py
+++ planetamovies/movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Movies
+
+
+class MoviesList(View):
+
+    def get_movie(self, request):
+        movies = Movies.objects.all()
+
+        return render(request, 'movies/movies.html', context={'movies': movies})
